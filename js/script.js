a81fd9749197c4322ef39b6afb83b349a6aee72c
loictonneau75 <loictonneau75@gmail.com>
2025-05-26 10:14:16 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ import * as DH from "./tools/domHelper.js";
 
 const backgroundDiv = backGround()
 document.body.appendChild(backgroundDiv)
-console.log(window.location.pathname)
+console.log(getBasePath())
 
 function backGround(){
     const video = DH.createCustomElement("video", {classList: ["css_background-video"], src: "./video/thé_background.mp4", type: "video/mp4", muted: true, autoplay: true, loop: true})
@@ -11,6 +11,11 @@ function backGround(){
     const div = DH.createCustomElement("div", {classList: ["css_background-div"]})
     div.append(video, overlay)
     return div
+}
+
+function getBasePath() {
+    const path = window.location.pathname;
+    return path.endsWith('/') ? path : path + '/';
 }
 
 async function getConfigValue() {
